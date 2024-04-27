0d54e51af9d66d5f86e521f3813f2e6b1f8aa7ed
Mahammed Javid <mahammedjavid8@gmail.com>
2024-04-27 14:35:29 +0530

    vuetify practice

---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout row>
+    <!-- justify-center flext-wrap -->
+      <v-flex xs12 md6>
+        <div class="caption grey--text">Project title</div>
+        <div>Create new website</div>
+      </v-flex>
+      <v-flex xs12 md12 lg-12>
+        <div class="caption grey--text">Project title</div>
+        <div>Create new website</div>
+      </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage'
+}
+</script>
